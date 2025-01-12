677842ca39ea83a57ee3e81db837f63e021ea6ed
Chukwuka Uba <promiseuba67@gmail.com>
2025-01-12 10:00:28 +0100

    Fixed issues with database connections and added new features for memory management, participant user state, and log entry creation

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,19 +1,19 @@
-import { PostgresDatabaseAdapter } from "@elizaos/adapter-postgres";
 import { SqliteDatabaseAdapter } from "@elizaos/adapter-sqlite";
 import Database from "better-sqlite3";
 import path from "path";
+import PostgresDatabaseAdapter from "../adapter-postgres/src";
 
 export function initializeDatabase(dataDir: string) {
   if (process.env.POSTGRES_URL) {
-    const db = new PostgresDatabaseAdapter({
+    const adapter = new PostgresDatabaseAdapter({
       connectionString: process.env.POSTGRES_URL,
     });
-    return db;
+    return adapter;
   } else {
     const filePath =
-      process.env.SQLITE_FILE ?? path.resolve(dataDir, "db.sqlite");
+      process.env.SQLITE_FILE ?? path.resolve(dataDir, "adapter.sqlite");
     // ":memory:";
-    const db = new SqliteDatabaseAdapter(new Database(filePath));
-    return db;
+    const adapter = new SqliteDatabaseAdapter(new Database(filePath));
+    return adapter;
   }
 }
