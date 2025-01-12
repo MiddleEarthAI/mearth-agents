d5e7d08e7391f8481bdc893098e68137d4bac7bd
Chukwuka Uba <promiseuba67@gmail.com>
2025-01-12 10:15:50 +0100

    update  database initialization to only use postgres db

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,6 @@
-import { SqliteDatabaseAdapter } from "@elizaos/adapter-sqlite";
-import Database from "better-sqlite3";
-import path from "path";
+// import { SqliteDatabaseAdapter } from "@elizaos/adapter-sqlite";
+// import Database from "better-sqlite3";
+// import path from "path";
 import PostgresDatabaseAdapter from "../adapter-postgres/src";
 
 export function initializeDatabase(dataDir: string) {
@@ -9,11 +9,14 @@ export function initializeDatabase(dataDir: string) {
       connectionString: process.env.POSTGRES_URL,
     });
     return adapter;
-  } else {
-    const filePath =
-      process.env.SQLITE_FILE ?? path.resolve(dataDir, "adapter.sqlite");
-    // ":memory:";
-    const adapter = new SqliteDatabaseAdapter(new Database(filePath));
-    return adapter;
   }
+  // }
+  // else {
+  //   // const filePath =
+  //   //   process.env.SQLITE_FILE ?? path.resolve(dataDir, "adapter.sqlite");
+  //   // // ":memory:";
+  //   // const adapter = new SqliteDatabaseAdapter(new Database(filePath));
+  //   // return adapter;
+  // }
+  throw new Error("POSTGRES_URL is not set");
 }
